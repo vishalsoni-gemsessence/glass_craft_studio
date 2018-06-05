878e81bba8384c3c33e3cb509482d202de4f2a54
Ameer <ameer.dev@gemsessence.com>
2018-06-05 20:17:28 +0530

    Added glass gift functionality

---
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -0,0 +1,27 @@
+module Spree
+  TaxonsController.class_eval do
+    helper 'spree/products'
+
+    respond_to :html
+
+    def show
+      @taxon = Spree::Taxon.find_by!(permalink: params[:id])
+      return unless @taxon
+
+      @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
+      @products = @searcher.retrieve_products
+
+      @childrens = @taxon.children.page(params[:page]).per(params[:per_page])
+    end
+
+    private
+
+    def accurate_title
+      if @taxon
+        @taxon.seo_title
+      else
+        super
+      end
+    end
+  end
+end
---
+++ app/overrides/taxon_decorator.rb
@@ -0,0 +1,3 @@
+Spree::Taxon.class_eval do
+  paginates_per 9
+end
