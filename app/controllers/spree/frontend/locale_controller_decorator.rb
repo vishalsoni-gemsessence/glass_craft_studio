9178a376724244455f1268d1803c651ca3e674ba
Ameer <ameer.dev@gemsessence.com>
2018-11-20 19:43:37 +0530

    change locale redirect path

---
+++ app/controllers/spree/frontend/locale_controller_decorator.rb
@@ -0,0 +1,15 @@
+Spree::LocaleController.class_eval do
+  def set
+    available_locales = Spree.i18n_available_locales
+    requested_locale = params[:switch_to_locale] || params[:locale]
+
+    if requested_locale && available_locales.map(&:to_s).include?(requested_locale)
+      session[:locale] = requested_locale
+      I18n.locale = requested_locale
+      flash.notice = t('spree.locale_changed')
+    else
+      flash[:error] = t('spree.locale_not_changed')
+    end
+    redirect_to request.referrer
+  end
+end
