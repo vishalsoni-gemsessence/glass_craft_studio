fe29f65fb7013f79a4117ff66992ec615bed4beb
Ameer <ameer.dev@gemsessence.com>
2018-11-22 19:31:49 +0530

    removed default url options for locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   
   before_action :set_locale
 
-  def default_url_options
-    { locale: I18n.locale }
-  end
+  # def default_url_options
+  #   { locale: I18n.locale }
+  # end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
