9737bfd5f1c4fdebcc6ba53d6749ad24d28870bc
Ameer <ameer.dev@gemsessence.com>
2018-12-18 21:01:32 +0530

    filter for photo gallery with reset and all btns

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -11,10 +11,17 @@ module Spree
       @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
       @products = @searcher.retrieve_products
 
+      # Use for Glass Photo Print
       @childrens = @taxon.children.page(params[:page]).per(params[:per_page])
-      if params[:taxon_children].present?
-        taxon_childrens = @taxon.children.find_by(name: params[:taxon_children])
-        @taxon_products = taxon_childrens.products.page(params[:page]).per(params[:per_page])
+
+      # Use for Filter in Photo Gallery page
+      if params[:selected_taxon].present? && !params[:selected_taxon].blank?
+        @taxon_products = []
+        params[:selected_taxon].each do |selected_taxon|
+          taxon_childrens = @taxon.children.find_by(name: selected_taxon)
+          @taxon_products.push(taxon_childrens.products)
+        end
+        @taxon_products = Kaminari.paginate_array(@taxon_products.flatten).page(params[:page]).per(params[:per_page])
       else
         @taxon_products = @taxon.products.page(params[:page]).per(params[:per_page])
       end
--- vendor/assets/javascripts/spree/frontend/js/custom.js
+++ vendor/assets/javascripts/spree/frontend/js/custom.js
@@ -10,10 +10,11 @@ var LoadMoreProductEvents = function(){
 var ready;
 
 ready = function() {
+  addActiveClass();
   LoadMoreProductEvents();
   scrollContactsForm();
   justifiedPhotoGallery();
-  addActiveClass();
+  LoadAllGalleryPhotos();
 };
 
 $(document).ready(ready);
@@ -35,7 +36,38 @@ function justifiedPhotoGallery(){
 }
 
 function addActiveClass(){
-  $('.search-btn').click(function(){
-    $(this).addClass('btn-active')
+  $('.filter-taxon-btn').click(function(e){
+      if($(this).hasClass('btn-active')){
+        $(this).removeClass('btn-active');
+      }
+      else{
+        $(this).addClass('btn-active');
+        $('.all-photos input').prop('checked', false);
+      }
+      var arr = []
+      $('.search-gallery').find('.filter-taxon-btn.btn-active').each(function(){
+          arr.push($(this).data('t_child'));
+      });
+
+      if(arr.length == 0){
+        $('.all-photos input').prop('checked', true);
+      }
+      $.ajax({
+        type: "GET",
+        url: "/t/photo-gallery",
+        dataType: 'script',
+        data: {
+          selected_taxon: arr
+        }
+      });
+    
+  });
+}
+
+function LoadAllGalleryPhotos(){
+  $('.all-photos input').click(function(){
+    $('.search-gallery').find('.filter-taxon-btn.btn-active').each(function(){
+      $(this).trigger('click');
+    });
   });
 }
