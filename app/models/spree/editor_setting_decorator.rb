8526a172fddea2818c2cc9ee6d46c91cd3d70a3f
Billy Cheng <billy@36etcetera.com>
2019-01-11 13:26:34 +0800

    TinyMCE config changes.

--- app/models/spree/editor_setting_decorator.rb
+++ app/models/spree/editor_setting_decorator.rb
@@ -1,7 +1,7 @@
 Spree::EditorSetting.class_eval do
   preference :enabled,        :boolean, default: true
   preference :current_editor, :string,  default: 'TinyMCE'
-  preference :ids,            :string,  default: 'product_description page_body taxon_description'
+  preference :ids,            :string,  default: 'product_description page_body taxon_description product_translations_attributes_1_description product_translations_attributes_2_description taxon_translations_attributes_1_description taxon_translations_attributes_2_description'
 
   def self.editors
     %w(TinyMCE CKEditor)
