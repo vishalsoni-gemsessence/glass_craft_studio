db276792ef9fcb0144c72d926514c1b3573bb96d
Billy Cheng <billy@36etcetera.com>
2019-01-17 14:14:36 +0800

    Debug confirmation email.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -33,7 +33,7 @@ Spree::Order.class_eval do
 
   def deliver_order_confirmation_email
     Spree::OrderMailer.confirm_email(self).deliver_later
-    update_column(:confirmation_delivered, true)
     Spree::OrderMailer.confirm_email_to_staffs(self).deliver_later
+    update_column(:confirmation_delivered, true)
   end
 end
