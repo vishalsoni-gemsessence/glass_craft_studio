b55ba30c341eb672198d80886549feb2ea53e359
Ameer <ameer.dev@gemsessence.com>
2019-01-21 15:33:05 +0530

    fixed product duplicate issue

--- app/models/spree/product_duplicator_decorator.rb
+++ app/models/spree/product_duplicator_decorator.rb
@@ -4,7 +4,7 @@ Spree::ProductDuplicator.class_eval do
     def duplicate_product
       product.dup.tap do |new_product|
         new_product.name = "COPY OF #{product.name}"
-        new_product.slug = new_product.slug + "-#{Time.now.to_i}"
+        new_product.slug = product.slug + "-#{Time.now.to_i}"
         new_product.taxons = product.taxons
         new_product.created_at = nil
         new_product.deleted_at = nil
@@ -12,6 +12,16 @@ Spree::ProductDuplicator.class_eval do
         new_product.product_properties = reset_properties
         new_product.master = duplicate_master
         new_product.variants = product.variants.map { |variant| duplicate_variant variant }
+      end
+    end
+
+    def duplicate_master
+      master = product.master
+      master.dup.tap do |new_master|
+        new_master.sku = "COPY OF #{master.sku} #{Time.now.to_i}"
+        new_master.deleted_at = nil
+        new_master.images = master.images.map { |image| duplicate_image image } if @include_images
+        new_master.price = master.price
       end
     end
 end
