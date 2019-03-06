747c6f7917655869c8b57a686f584bbdb7d97d2c
Billy Cheng <billy@36etcetera.com>
2019-03-06 17:58:14 +0800

    more obsolete migration code.

--- db/migrate/20181113141456_add_translation_to_sticker.solidus_on_demand_products.rb
+++ db/migrate/20181113141456_add_translation_to_sticker.solidus_on_demand_products.rb
@@ -1,13 +1,13 @@
 # This migration comes from solidus_on_demand_products (originally 20181113141028)
 class AddTranslationToSticker < ActiveRecord::Migration[5.1]
   def up
-    unless table_exists?(:spree_sticker_translations)
-      params = { name: :string }
-      Spree::Sticker.create_translation_table!(params, { migrate_data: true })
-    end
+    # unless table_exists?(:spree_sticker_translations)
+    #   params = { name: :string }
+    #   Spree::Sticker.create_translation_table!(params, { migrate_data: true })
+    # end
   end
 
   def down
-    Spree::Sticker.drop_translation_table! migrate_data: true
+    # Spree::Sticker.drop_translation_table! migrate_data: true
   end
 end
