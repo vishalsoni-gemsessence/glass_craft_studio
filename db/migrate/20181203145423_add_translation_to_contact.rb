6feb9c40b570935d532d8bb6612d4bd7a942d72a
Billy Cheng <billy@36etcetera.com>
2019-03-06 18:09:30 +0800

    obsolete codes in migration script

--- db/migrate/20181203145423_add_translation_to_contact.rb
+++ db/migrate/20181203145423_add_translation_to_contact.rb
@@ -1,12 +1,12 @@
 class AddTranslationToContact < ActiveRecord::Migration[5.1]
   def up
-    unless table_exists?(:spree_contact_translations)
-      params = { name: :string, slug: :string, subject: :string, message: :text }
-      Spree::Contact.create_translation_table!(params, { migrate_data: true })
-    end
+    # unless table_exists?(:spree_contact_translations)
+    #   params = { name: :string, slug: :string, subject: :string, message: :text }
+    #   Spree::Contact.create_translation_table!(params, { migrate_data: true })
+    # end
   end
 
   def down
-    Spree::Contact.drop_translation_table! migrate_data: true
+    # Spree::Contact.drop_translation_table! migrate_data: true
   end
 end
--- db/migrate/20190104120904_remove_translation_from_editor_template.solidus_on_demand_products.rb
+++ db/migrate/20190104120904_remove_translation_from_editor_template.solidus_on_demand_products.rb
@@ -1,10 +1,10 @@
 # This migration comes from solidus_on_demand_products (originally 20181231101642)
 class RemoveTranslationFromEditorTemplate < ActiveRecord::Migration[5.1]
   def up
-    Spree::Template.drop_translation_table! migrate_data: true
-    Spree::Filter.drop_translation_table! migrate_data: true
-    Spree::Sticker.drop_translation_table! migrate_data: true
-    Spree::Contact.drop_translation_table! migrate_data: true
+    # Spree::Template.drop_translation_table! migrate_data: true
+    # Spree::Filter.drop_translation_table! migrate_data: true
+    # Spree::Sticker.drop_translation_table! migrate_data: true
+    # Spree::Contact.drop_translation_table! migrate_data: true
   end
 
   def down
