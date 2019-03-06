6583adc10e53204f9030a8e682cab4ff5e37de9a
Billy Cheng <billy@36etcetera.com>
2019-03-06 18:05:38 +0800

    obsolete code in migration script

--- db/migrate/20181121102823_add_slug_to_extension_translated_table.solidus_on_demand_products.rb
+++ db/migrate/20181121102823_add_slug_to_extension_translated_table.solidus_on_demand_products.rb
@@ -1,14 +1,14 @@
 # This migration comes from solidus_on_demand_products (originally 20181121093909)
 class AddSlugToExtensionTranslatedTable < ActiveRecord::Migration[5.1]
   def up
-    Spree::Template.add_translation_fields! slug: :string
-    Spree::Sticker.add_translation_fields! slug: :string
-    Spree::Filter.add_translation_fields! slug: :string
+    # Spree::Template.add_translation_fields! slug: :string
+    # Spree::Sticker.add_translation_fields! slug: :string
+    # Spree::Filter.add_translation_fields! slug: :string
   end
 
   def down
-    remove_column :spree_template_translations, :slug
-    remove_column :spree_sticker_translations, :slug
-    remove_column :spree_filter_translations, :slug
+    # remove_column :spree_template_translations, :slug
+    # remove_column :spree_sticker_translations, :slug
+    # remove_column :spree_filter_translations, :slug
   end
 end
