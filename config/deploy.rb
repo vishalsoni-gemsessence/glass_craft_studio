708e66f502d83b8cc4d5ad9ca1b5b10e591de2fa
Billy Cheng <billy@36etcetera.com>
2019-03-07 14:54:07 +0800

    fix syntax and deployment settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,5 +38,3 @@ set :keep_releases, 5
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-# To restart passenger using `touch tmp/restart.txt`
-set :passenger_restart_with_touch, true
