7892ef573273d56aa3c7785a63dc9581343a6463
Billy Cheng <billy@36etcetera.com>
2019-04-01 11:41:53 +0800

    IP detection testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :set_locale
-
+  before_action :detect_origin
+  
   # def default_url_options
   #   { locale: I18n.locale }
   # end
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
+  def detect_origin
+    remote_ip = request.remote_ip
+    results = Geocoder.search(remote_ip)
+    @current_country = Country.new(results.first.country)
+    @current_ip_address = remote_ip
   end
 end
