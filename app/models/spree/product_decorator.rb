32ad565d8daae0673ed0f3ee4864613e87da2df0
Billy Cheng <billy@36etcetera.com>
2019-04-04 11:55:36 +0800

    Break product name into multiple lines.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -6,4 +6,21 @@ Spree::Product.class_eval do
     where("spree_products_taxons.taxon_id" => taxon.id).
     order(Spree::Classification.arel_table[:position].asc)
   end
+  
+  def display_product_name
+    splitted_names = name.split(' ')
+    final_name = ''
+    temp_name = ''
+    splitted_names.each do |token|
+      temp_name << "#{token} "
+      puts "temp_name ===> #{temp_name} | final_name ====> #{final_name}"
+      if temp_name.length > 31
+        final_name = "#{final_name.strip}<br>#{token}"
+        temp_name = "#{token}"
+      else
+        final_name << "#{token} "
+      end
+    end
+    return final_name.strip
+  end
 end
