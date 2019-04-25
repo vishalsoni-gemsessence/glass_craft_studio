60dd0af8898c95e1227c6814aaf810745300803c
Billy Cheng <billy@36etcetera.com>
2019-04-25 12:07:55 +0800

    Add a condition to handle nil class error

--- app/models/spree/validations/db_maximum_length_validator_decorator.rb
+++ app/models/spree/validations/db_maximum_length_validator_decorator.rb
@@ -2,9 +2,9 @@ Spree::Validations::DbMaximumLengthValidator.class_eval do
   ##
   # Validates a field based on the maximum length of the underlying DB field, if there is one.
   def validate(record)
-    return if @field.nil?
-    puts "======== [DbMaximumLengthValidator] Field: #{@field}."
-    puts "======== [DbMaximumLengthValidator] Record: #{record.class}."
+    return if @field.nil? || record.class.columns_hash[@field].nil?
+    Rails.logger.debug "======== [DbMaximumLengthValidator] Field: #{@field}."
+    Rails.logger.debug "======== [DbMaximumLengthValidator] Record: #{record.class}."
     limit = record.class.columns_hash[@field].limit
     value = record[@field.to_sym]
     if value && limit && value.to_s.length > limit
