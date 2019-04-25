e713a572e201c6427a68a2bfa842eea645f93386
Billy Cheng <billy@36etcetera.com>
2019-04-25 14:16:03 +0800

    Add debug messages.

--- app/models/spree/validations/db_maximum_length_validator_decorator.rb
+++ app/models/spree/validations/db_maximum_length_validator_decorator.rb
@@ -3,8 +3,9 @@ Spree::Validations::DbMaximumLengthValidator.class_eval do
   # Validates a field based on the maximum length of the underlying DB field, if there is one.
   def validate(record)
     return if @field.nil?
-    puts "======== [DbMaximumLengthValidator] Field: #{@field}."
-    puts "======== [DbMaximumLengthValidator] Record: #{record.class}."
+    Rails.logger.debug "======== [DbMaximumLengthValidator] Field: #{@field}."
+    Rails.logger.debug "======== [DbMaximumLengthValidator] Record: #{record.class}."
+    Rails.logger.debug "======== [DbMaximumLengthValidator] Column Hash: #{record.class.columns_hash[@field]}."
     limit = record.class.columns_hash[@field].limit
     value = record[@field.to_sym]
     if value && limit && value.to_s.length > limit
