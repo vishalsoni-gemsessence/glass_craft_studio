8add099395a360586c12b26bc706fd162a2f0ddd
Billy Cheng <billy@36etcetera.com>
2019-05-09 10:38:54 +0800

    Remove the flash message after changed the locale.

--- app/controllers/spree/frontend/locale_controller_decorator.rb
+++ app/controllers/spree/frontend/locale_controller_decorator.rb
@@ -6,9 +6,9 @@ Spree::LocaleController.class_eval do
     if requested_locale && available_locales.map(&:to_s).include?(requested_locale)
       session[:locale] = requested_locale
       I18n.locale = requested_locale
-      flash.notice = t('spree.locale_changed')
-    else
-      flash[:error] = t('spree.locale_not_changed')
+      # flash.notice = t('spree.locale_changed')
+    # else
+      # flash[:error] = t('spree.locale_not_changed')
     end
     redirect_to request.referrer
   end
