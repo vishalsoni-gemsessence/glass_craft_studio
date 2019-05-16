fb575ce1c4603d6816f02e2d32f6bd7382ae37ea
Billy Cheng <billy@36etcetera.com>
2019-05-16 13:34:33 +0800

    Fixed the problem when duplicating a product, the slug in non-current locale is not properly generated. Still retaining the original product slug that make the link generation incorrect.

--- app/models/spree/product_duplicator_decorator.rb
+++ app/models/spree/product_duplicator_decorator.rb
@@ -1,27 +1,32 @@
 Spree::ProductDuplicator.class_eval do
-    protected
+            
+  protected
 
-    def duplicate_product
-      product.dup.tap do |new_product|
-        new_product.name = "COPY OF #{product.name}"
-        new_product.slug = product.slug + "-#{Time.now.to_i}"
-        new_product.taxons = product.taxons
-        new_product.created_at = nil
-        new_product.deleted_at = nil
-        new_product.updated_at = nil
-        new_product.product_properties = reset_properties
-        new_product.master = duplicate_master
-        new_product.variants = product.variants.map { |variant| duplicate_variant variant }
+  def duplicate_product
+    product.dup.tap do |new_product|
+      I18n.available_locales.each do |locale|
+        I18n.with_locale(locale) {
+          new_product.name = "#{I18n.t('spree.copy_of')} #{product.name}"
+          new_product.slug = product.slug + "-#{Time.now.to_i}"  
+        }
       end
+      new_product.taxons = product.taxons
+      new_product.created_at = nil
+      new_product.deleted_at = nil
+      new_product.updated_at = nil
+      new_product.product_properties = reset_properties
+      new_product.master = duplicate_master
+      new_product.variants = product.variants.map { |variant| duplicate_variant variant }
     end
+  end
 
-    def duplicate_master
-      master = product.master
-      master.dup.tap do |new_master|
-        new_master.sku = "COPY OF #{master.sku} #{Time.now.to_i}"
-        new_master.deleted_at = nil
-        new_master.images = master.images.map { |image| duplicate_image image } if @include_images
-        new_master.price = master.price
-      end
+  def duplicate_master
+    master = product.master
+    master.dup.tap do |new_master|
+      new_master.sku = "#{I18n.t('spree.copy_of')} #{master.sku} #{Time.now.to_i}"
+      new_master.deleted_at = nil
+      new_master.images = master.images.map { |image| duplicate_image image } if @include_images
+      new_master.price = master.price
     end
+  end
 end
