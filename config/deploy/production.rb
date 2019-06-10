46e5c522b26071d0f00b029eee2805f4b85657a7
Billy Cheng <billy@36etcetera.com>
2019-06-10 14:03:08 +0800

    Update image editor

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,37 +1,29 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+# Define the rails env
+set :rails_env, 'production'
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deploy/apps/glassxpert'
 
+# Default tmp_dir directory is /var/www/tmp
+set :tmp_dir, "/home/deploy/apps/tmp"
 
+# Set the rvm ruby version
+set :rvm_ruby_version, 'ruby-2.5.0@glassxpert'
 
-# role-based syntax
-# ==================
+# Set the bundler to skip the listed env from Gemfile
+set :bundle_without, %w{development test}.join(' ')
 
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
+# To restart passenger using `touch tmp/restart.txt`
+set :passenger_restart_with_touch, true
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+# Bitbucket Branch
+set :branch, 'master' # e.g. 'master'
 
 
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
+append :linked_files, "tmp/restart.txt"
 
 
+server '178.79.137.34', user: 'deploy', roles: %w{app db web}
 
 # Custom SSH Options
 # ==================
@@ -41,21 +33,9 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+set :ssh_options, {
+  #  keys: %w(/home/rlisowski/.ssh/id_rsa),
+  forward_agent: false,
+  auth_methods: %w(password),
+  password: "Abcde!2345"  
+}
