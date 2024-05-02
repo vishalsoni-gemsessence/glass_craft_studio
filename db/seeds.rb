1853e44b5e05156e326634aa4effcd6807fe7e4e
VIjay Chouhan <vijaychouhan.rails@gmail.com>
2024-05-02 13:58:25 +0530

    minor changes for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -174,7 +174,7 @@ def upload_stickers
 end
 
   Spree::User.destroy_all
-  
+
   user = Spree::User.create!(email: "admin@example.com", password: "P@ssword")
 
   admin_role = Spree::Role.find_or_create_by!(name: "admin")
@@ -186,7 +186,6 @@ end
   admin_role = Spree::Role.find_or_create_by!(name: "admin")
 
   user.spree_roles << admin_role
-end
 
 def create_templates
   # template
